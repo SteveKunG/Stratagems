fd567c6f132f21162baaa1ccabd65367d291e4b6
SteveKunG <mccommander_minecraft@hotmail.com>
2024-07-25 17:08:37 +0700

    Added can_depleted property

--- src/main/java/com/stevekung/stratagems/StratagemProperties.java
+++ src/main/java/com/stevekung/stratagems/StratagemProperties.java
@@ -7,13 +7,14 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.util.ExtraCodecs;
 
-public record StratagemProperties(int incomingDuration, Optional<Integer> duration, int cooldown, Optional<Integer> remainingUse, int beamColor)
+public record StratagemProperties(int incomingDuration, Optional<Integer> duration, int cooldown, Optional<Integer> remainingUse, int beamColor, Optional<Boolean> canDepleted)
 {
     public static final Codec<StratagemProperties> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ExtraCodecs.NON_NEGATIVE_INT.fieldOf("incoming_duration").forGetter(StratagemProperties::incomingDuration),
             ExtraCodecs.NON_NEGATIVE_INT.optionalFieldOf("duration").forGetter(StratagemProperties::duration),
             ExtraCodecs.NON_NEGATIVE_INT.fieldOf("cooldown").forGetter(StratagemProperties::cooldown),
             ExtraCodecs.NON_NEGATIVE_INT.optionalFieldOf("remaining_use").forGetter(StratagemProperties::remainingUse),
-            Codec.INT.fieldOf("beam_color").forGetter(StratagemProperties::beamColor)
+            Codec.INT.fieldOf("beam_color").forGetter(StratagemProperties::beamColor),
+            Codec.BOOL.optionalFieldOf("can_depleted").forGetter(StratagemProperties::canDepleted)
             ).apply(instance, StratagemProperties::new));
 }
--- src/main/java/com/stevekung/stratagems/registry/Stratagems.java
+++ src/main/java/com/stevekung/stratagems/registry/Stratagems.java
@@ -33,23 +33,23 @@ public class Stratagems
 
     public static void bootstrap(BootstrapContext<Stratagem> context)
     {
-        register(context, REINFORCE, "wsdaw", new ItemStack(Items.RESPAWN_ANCHOR), ReinforceAction.reinforce(), ReinforceStratagemRule.defaultRule(), 0, 2400, 20, BLUE_COLOR);
+        register(context, REINFORCE, "wsdaw", new ItemStack(Items.RESPAWN_ANCHOR), ReinforceAction.reinforce(), ReinforceStratagemRule.defaultRule(), 0, 2400, 20, BLUE_COLOR, Optional.empty());
         register(context, BOW, "ssawd", new ItemStack(Items.BOW), SpawnItemAction.spawnItems(new ItemStack(Items.BOW), new ItemStack(Items.ARROW, 64)), 100, 6000, BLUE_COLOR);
         register(context, SUPPLY_CHEST, "sswd", new ItemStack(Items.CHEST), SpawnSupplyAction.spawnSupply(BuiltInLootTables.SPAWN_BONUS_CHEST), 200, 6000, BLUE_COLOR);
         register(context, IRON_SWORD, "saswd", new ItemStack(Items.IRON_SWORD), SpawnItemAction.spawnItem(new ItemStack(Items.IRON_SWORD)), 100, 6000, BLUE_COLOR);
         register(context, IRON_PICKAXE, "saswwd", new ItemStack(Items.IRON_PICKAXE), SpawnItemAction.spawnItem(new ItemStack(Items.IRON_PICKAXE)), 200, 6000, BLUE_COLOR);
         register(context, BLOCK, "wdsd", new ItemStack(Items.STONE), SpawnItemAction.spawnItem(new ItemStack(Items.STONE, 64)), 100, 1200, BLUE_COLOR);
-        register(context, TNT, "swaswdsw", new ItemStack(Items.TNT), SpawnBombAction.spawnBomb(40), DefaultStratagemRule.defaultRule(), 400, 12000, 3, RED_COLOR);
+        register(context, TNT, "swaswdsw", new ItemStack(Items.TNT), SpawnBombAction.spawnBomb(40), DefaultStratagemRule.defaultRule(), 400, 12000, 3, RED_COLOR, Optional.of(true));
     }
 
-    static void register(BootstrapContext<Stratagem> context, ResourceKey<Stratagem> key, String code, ItemStack icon, StratagemAction.Builder action, StratagemRule.Builder rule, int incomingDuration, int cooldown, int remainingUse, int beamColor)
+    static void register(BootstrapContext<Stratagem> context, ResourceKey<Stratagem> key, String code, ItemStack icon, StratagemAction.Builder action, StratagemRule.Builder rule, int incomingDuration, int cooldown, int remainingUse, int beamColor, Optional<Boolean> canDepleted)
     {
-        context.register(key, new Stratagem(code, Component.translatable(key.location().toLanguageKey("stratagem")), Either.left(icon), action.build(), rule.build(), new StratagemProperties(incomingDuration, Optional.empty(), cooldown, Optional.of(remainingUse), beamColor)));
+        context.register(key, new Stratagem(code, Component.translatable(key.location().toLanguageKey("stratagem")), Either.left(icon), action.build(), rule.build(), new StratagemProperties(incomingDuration, Optional.empty(), cooldown, Optional.of(remainingUse), beamColor, canDepleted)));
     }
 
     static void register(BootstrapContext<Stratagem> context, ResourceKey<Stratagem> key, String code, ItemStack icon, StratagemAction.Builder action, int incomingDuration, int cooldown, int beamColor)
     {
-        context.register(key, new Stratagem(code, Component.translatable(key.location().toLanguageKey("stratagem")), Either.left(icon), action.build(), DefaultStratagemRule.defaultRule().build(), new StratagemProperties(incomingDuration, Optional.empty(), cooldown, Optional.empty(), beamColor)));
+        context.register(key, new Stratagem(code, Component.translatable(key.location().toLanguageKey("stratagem")), Either.left(icon), action.build(), DefaultStratagemRule.defaultRule().build(), new StratagemProperties(incomingDuration, Optional.empty(), cooldown, Optional.empty(), beamColor, Optional.empty())));
     }
 
     private static ResourceKey<Stratagem> createKey(String name)
