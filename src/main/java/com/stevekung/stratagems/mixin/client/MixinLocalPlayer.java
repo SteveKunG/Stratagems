73f7a2f311ed580471225215e7da032018575473
SteveKunG <mccommander_minecraft@hotmail.com>
2024-11-05 12:24:44 +0700

    Move client mixin to its package

--- src/main/java/com/stevekung/stratagems/mixin/client/MixinLocalPlayer.java
+++ src/main/java/com/stevekung/stratagems/mixin/client/MixinLocalPlayer.java
@@ -1,4 +1,4 @@
-package com.stevekung.stratagems.mixin;
+package com.stevekung.stratagems.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
